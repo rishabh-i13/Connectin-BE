8d863b7fd2bb853e4f3e44a130a303bd1c480635
Rishabh Shukla <rishabhshukla1333@gmail.com>
2025-05-18 02:20:33 +0530

    FE hosted origin added

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT=process.env.PORT || 5000;
 const app=express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://connectin-fe.onrender.com"],
     credentials: true,
 }));
 
